ade63eebd9ee4aa4e80837a213392cff6d9e7d91
MasterHiggins <151066454+MasterHiggins@users.noreply.github.com>
2024-08-02 00:07:59 -0230

    Mongo com

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const session = require('express-session')
+const session = require('express-session');
 require('dotenv').config();
-const PORT = process.env.PORT || 3000
-global.DEBUG = true
+const PORT = process.env.PORT || 3000;
+global.DEBUG = true;
+const db = require('./services/db'); //Ensure database initialization
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
 }));
 
-app.listen(PORT,(err)=>{
-    if(err)console.log(err)
-    console.log(`app on ${PORT}`)
-})
-//note to add event emiters .get's need to be async
-app.get('/',(req,res)=>{
-    res.render('index',{stat: req.session.stat})
-})
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log(`app on ${PORT}`);
+});
 
-const loginRouter = require('./routes/login')
-app.use("/login",loginRouter)
+app.get('/', (req, res) => {
+  res.render('index', { stat: req.session.stat });
+});
 
+const loginRouter = require('./routes/login');
+app.use("/login", loginRouter);
 
-const searchRouter = require('./routes/search')
-app.use('/search',searchRouter)
+const searchRouter = require('./routes/search');
+app.use('/search', searchRouter);
