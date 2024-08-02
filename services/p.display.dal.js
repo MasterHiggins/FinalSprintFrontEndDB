1930a8718b1d6c2aea45f3925aa1a9908275aa07
MasterHiggins <151066454+MasterHiggins@users.noreply.github.com>
2024-08-02 00:05:46 -0230

    Mongo Compatibility

--- services/p.display.dal.js
+++ services/p.display.dal.js
@@ -1,19 +1,32 @@
-const dal = require('./db')
+const db = require('./db');
+const Product = require('../models/product'); //Import the MongoDB Product model
 
-const getInfo = function(text) {
-    if(DEBUG) console.log('in display.dal.js')
-    return new Promise(function(resolve,reject){
-        const sql = `select * from products where description iLIKE '%'||$1||'%' or name iLIKE '%'||$1||'%' or cast(price as text) iLIKE '%'||$1||'%'`;
-dal.query(sql, [text], (err,res)=>{
-    if(err){
-        //logging
-        reject(err)
-    }else{
-        resolve(res.rows)
-    }
-})
-})
-}
-module.exports={
-    getInfo,
-}
+const useMongoDB = process.env.USE_MONGO_DB === 'true';
+
+const getInfo = async function(text) {
+  if (DEBUG) console.log('in display.dal.js');
+  if (useMongoDB) {
+    return Product.find({ $or: [
+      { description: { $regex: text, $options: 'i' } },
+      { name: { $regex: text, $options: 'i' } },
+      { price: { $regex: text, $options: 'i' } }
+    ] }).exec();
+  } else {
+    const sql = `select * from products where description iLIKE '%'||$1||'%' or name iLIKE '%'||$1||'%' or cast(price as text) iLIKE '%'||$1||'%'`;
+    return new Promise(function(resolve, reject) {
+      db.query(sql, [text], (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.rows);
+        }
+      });
+    });
+  }
+};
+
+module.exports = {
+  getInfo,
+};
+
+//Suffering :(
