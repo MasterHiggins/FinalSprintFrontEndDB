418783e130416aeb0b0aef30ac0c082759b1fa9a
MasterHiggins <151066454+MasterHiggins@users.noreply.github.com>
2024-08-02 00:04:40 -0230

    Mongo Compatibility

--- services/db.js
+++ services/db.js
@@ -6,4 +6,23 @@ const pool = new Pool({
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT
 })
+const { Pool } = require('pg');
+const mongoose = require('mongoose');
+
+const useMongoDB = process.env.USE_MONGO_DB === 'true';
+
+let db;
+
+if (useMongoDB) {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+  db = mongoose;
+} else {
+  db = new Pool({
+    connectionString: process.env.PG_URI,
+  });
+}
+
+module.exports = db;
 module.exports = pool;
