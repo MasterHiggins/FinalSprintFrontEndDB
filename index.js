c1922056e36d47ab41a87b559a1b7c9f8ac42fdd
Maz Radwan <maz.radwan@keyin.com>
2024-08-04 20:15:02 -0230

    tests added

--- index.js
+++ index.js
@@ -170,12 +170,13 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
-// Start server (only once)
-app.listen(PORT, (err) => {
-  if (err) console.log(err);
-  emitter.emit("log", "server", `Server is running on port ${PORT}`);
-  logger.info(`Server is running on port ${PORT}`);
-  console.log(`Server is running on port ${PORT}`);
-});
-
+if (process.env.NODE_ENV !== "test") {
+  // Start server (only once)
+  app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    emitter.emit("log", "server", `Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 module.exports = app;
